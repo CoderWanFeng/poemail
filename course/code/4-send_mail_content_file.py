46bb9a278b51d510adfb830f2806458f1c0a72ae
Administrator <1639972003@qq.com>
2025-05-17 17:36:37 +0800

    删除注释

--- course/code/4-send_mail_content_file.py
+++ course/code/4-send_mail_content_file.py
@@ -1,11 +1,3 @@
-# -*- coding: UTF-8 -*-
-'''
-@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫，微信：CoderWanFeng
-@读者群     ：http://www.python4office.cn/wechat-group/
-@学习网站      ：https://www.python-office.com
-@代码日期    ：2023/12/24 1:26 
-@本段代码的视频说明     ：
-'''
 import os
 from datetime import datetime
 
@@ -26,11 +18,3 @@ poemail.send.send_email(key=key,
                         content='测试邮件发送' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
                         attach_files=attach_files)
 
-# import office
-#
-# office.email.send_email(key=key,
-#                         msg_from=msg_from,
-#                         msg_to=msg_to,
-#                         msg_cc=msg_cc,
-#                         msg_subject='带附件的邮件' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
-#                         content='测试邮件发送' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
